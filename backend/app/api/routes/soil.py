95f2b659598883c96358835805b4feb11ef53484
Natthapol SERMSARAN <natthapol.se@ku.th>
2024-05-12 16:46:59 +0700

    Create endpoint get soil for specified date and hour

--- backend/app/api/routes/soil.py
+++ backend/app/api/routes/soil.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 
 from app.api.deps import get_db
 from app.models.kidbright import Kidbright
@@ -18,3 +21,28 @@ def get_current_soil(db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Soil data not found")
 
     return current_soil
+
+
+@router.get(
+    "/hour/{specific_date}/{hour}",
+    response_model=SoilBase,
+    summary="Returns soil details for a specified date and hour",
+)
+def get_soil_by_hour(specific_date: date, hour: int, db: Session = Depends(get_db)):
+    soil_data = (
+        db.query(Kidbright)
+        .filter(
+            func.date(Kidbright.ts) == specific_date,
+            func.extract("hour", Kidbright.ts) == hour,
+        )
+        .order_by(Kidbright.ts.asc())
+        .first()
+    )
+
+    if not soil_data:
+        raise HTTPException(
+            status_code=404,
+            detail="Soil data not found for the specified date and hour",
+        )
+
+    return soil_data
