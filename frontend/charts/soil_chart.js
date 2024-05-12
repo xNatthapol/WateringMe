2f6475c354dc870c44b42e9bc1e7e28bd9f1f7f8
Natthapol SERMSARAN <natthapol.se@ku.th>
2024-05-13 01:58:35 +0700

    Implement front-end visualization create dashboard and soil chart

---
+++ frontend/charts/soil_chart.js
@@ -0,0 +1,73 @@
+import { fetchData, getCurrentDateFormatted } from "./utils.js";
+
+async function createChart() {
+  try {
+    const currentDate = getCurrentDateFormatted();
+
+    // Fetch historical soil moisture data
+    const historicalData = await fetchData(
+      `http://127.0.0.1:8000/api/v1/soil/day/${currentDate}`,
+    );
+    // Fetch forecast soil moisture data
+    const forecastData = await fetchData(
+      "http://127.0.0.1:8000/api/v1/soil/forecast",
+    );
+
+    // Add last historical point to forecast data for continuity
+    if (historicalData.length > 0 && forecastData.length > 0) {
+      forecastData.unshift({
+        ts: historicalData[historicalData.length - 1].ts, // last historical timestamp
+        sm: historicalData[historicalData.length - 1].sm, // last historical soil moisture
+      });
+    }
+
+    // Prepare forecast data for plotting
+    const forecastTrace = {
+      x: forecastData.map((data) => data.ts),
+      y: forecastData.map((data) => data.sm),
+      type: "scatter",
+      mode: "lines+markers",
+      name: "Forecast Soil Moisture",
+      line: { color: "green" },
+    };
+
+    // Prepare historical data for plotting
+    const historicalTrace = {
+      x: historicalData.map((data) => data.ts),
+      y: historicalData.map((data) => data.sm),
+      type: "scatter",
+      mode: "lines+markers",
+      name: "Historical Soil Moisture",
+      line: { color: "black" },
+    };
+
+    // Calculate min and max for Y-axis
+    const allValues = historicalData
+      .concat(forecastData)
+      .map((data) => data.sm);
+    const minValue = Math.min(...allValues);
+    const maxValue = Math.max(...allValues);
+
+    // Plot layout configuration
+    var layout = {
+      title: "Soil Moisture Data Visualization",
+      xaxis: {
+        title: "Timestamp",
+        type: "date",
+      },
+      yaxis: {
+        title: "Soil Moisture (%)",
+        dtick: 1.0,
+        range: [minValue - 3, maxValue + 3],
+      },
+      autosize: true,
+    };
+
+    // Render the plot
+    Plotly.newPlot("chart", [forecastTrace, historicalTrace], layout);
+  } catch (error) {
+    console.error("Error fetching or plotting data:", error);
+  }
+}
+
+createChart();
---
+++ frontend/charts/utils.js
@@ -0,0 +1,17 @@
+// Fetch data from url
+export async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+}
+
+// Get Current date
+export function getCurrentDateFormatted() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = (today.getMonth() + 1).toString().padStart(2, "0");
+  const day = today.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
