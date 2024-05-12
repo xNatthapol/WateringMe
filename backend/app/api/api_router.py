aad9fabe4a7f9e34f47576726c970ceeb1ad466b
Natthadit LERTPISANWUT <natthadit.l@ku.th>
2024-05-12 17:19:04 +0700

    create perdicted PET endpoint

--- backend/app/api/api_router.py
+++ backend/app/api/api_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .routes import soil, weather
+from .routes import soil, weather, pet
 
 api_router = APIRouter()
 
 api_router.include_router(soil.router)
 api_router.include_router(weather.router)
+api_router.include_router(pet.router)
---
+++ backend/app/api/routes/pet.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
+
+from app.api.deps import get_db
+from app.models.pet import PET
+from app.schemas.pet import PETBase
+
+router = APIRouter(prefix="/pet", tags=["Pet"])
+
+@router.get(
+    "/forecast",
+    response_model=PETBase,
+    summary="Returns PET at the current day",
+)
+def get_pet_per_day(db: Session = Depends(get_db)):
+    forecast_pet = (db.query(PET.ts, PET.lat, PET.lon, PET.pet)
+                    .order_by(PET.ts.desc())
+                    .first())
+
+    if not forecast_pet:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+
+    return forecast_pet
